90db021b4ef4e0e4e98abe62167a933f80d482c5
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-01-21 18:07:16 -0500

    added jquery to first div

---
+++ app/assets/javascripts/home_pages.js
@@ -0,0 +1,19 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function(){
+     var child = $(".resume_white");
+    $(".resume_red").hover(function(){
+       
+       child.animate({"top": '0px',"left": '0px'},500,function(){
+            $(this).css({"opacity": "0.4" });
+           $(".resume_red p").css({"visibility": "visible"});
+        });
+      },
+   function(){ $(".resume_red p").css({"visibility": "hidden"}), child.animate({"top": '53.75px',"left": '105px'},500,function(){
+            $(this).css({"opacity": "1" });
+        });
+   });
+
+});
