c51dea8ebac5e47d75fb3ef8c4aaa740474b212c
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-01-23 15:26:48 -0500

    added mailer ENV

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,7 +30,15 @@ PersonalSite::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
-
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            ENV["GMAIL_USERNAME"],
+  password:             ENV["GMAIL_PASSWORD"],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
 end
