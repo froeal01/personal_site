e32711dcba887772bb139575438c78fa74b67634
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-01-23 16:15:42 -0500

    deleted db file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131227214242) do
+ActiveRecord::Schema.define(version: 20140123210757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20131227214242) do
   create_table "home_pages", force: true do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "body"
+    t.string   "subject"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
