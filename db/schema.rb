8f7247631a32fb369c7caa5a923ccc68c4d529f4
Alex Froelich <alexfroelich@Alexs-MacBook-Pro.local>
2014-02-10 14:42:39 -0500

    seed file removed

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20140206224407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "home_pages", force: true do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "javascript_projects", force: true do |t|
     t.string "name"
     t.string "description"
